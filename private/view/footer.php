2b95129dc952d228f9d8674c7b81dd3631ecd87f
lionel <lionel.lft@me.com>
2017-02-14 23:54:02 +0100

    change jquery cdn and form padding

--- private/view/footer.php
+++ private/view/footer.php
@@ -12,7 +12,7 @@
             <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="blank">CC BY-NC-SA 4.0</a> - 2017 | Réalisé par <a href="http://www.cooldev.xyz">Lionel</a> pour <a href="http://www.mpaw.xyz" target="blank">MPAW </a>
             | <a href="ml.php">Mentions légales</a><img src="public/images/pinapple-icon.png" width="30rem"></p>
         </footer>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
         <script type="text/javascript" src="public/js/main.js"></script>
     </body>
 </html>
