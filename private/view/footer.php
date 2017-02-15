ecd9dd07318411da4558797a2bc74e7be84ebb19
llafont <lionel.lft@me.com>
2017-02-15 23:34:26 +0100

    fixed responsive bugs for tablet and desktop

--- private/view/footer.php
+++ private/view/footer.php
@@ -9,8 +9,8 @@
                 </div>
             </section>
             <p><img src="public/images/pinapple-icon.png" width="30rem">
-            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="blank">CC BY-NC-SA 4.0</a> - 2017 | Réalisé par <a href="http://www.cooldev.xyz">Lionel</a> pour <a href="http://www.mpaw.xyz" target="blank">MPAW </a>
-            | <a href="ml.php">Mentions légales</a><img src="public/images/pinapple-icon.png" width="30rem"></p>
+            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="blank">CC BY-NC-SA 4.0</a> - 2017 | Réalisé par <a href="http://www.cooldev.xyz">Lionel</a> pour <a href="http://www.mpaw.xyz" target="blank">MPAW </a><img src="public/images/pinapple-icon.png" width="30rem"></br>
+            <a href="ml.php">Mentions légales</a></p>
         </footer>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
         <script type="text/javascript" src="public/js/main.js"></script>
--- private/view/header.php
+++ private/view/header.php
@@ -3,10 +3,11 @@
     <?php require_once('nav.php'); ?>
     <div id="logo"><a href="index.php"><img src="public/images/logo.svg"></a></div>
     <div id="tagline">
-        <h1>L'agence dont vous êtes le <img src="public/images/heros.svg"></img></h1>
+        <h1>L'agence dont vous êtes le</h1>
+        <img src="public/images/heros.svg"></img>
     </div>
     <div id="btn-scroll" class="bounce">
         <a class="scrollTo" href="#public">En savoir plus ;)</a>
     </div>
-    
+
 </header>
