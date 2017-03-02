7fe4146ac6c0737004fd65d68cf9a9b9c74f84a9
llafont <lionel.lft@me.com>
2017-03-02 21:16:43 +0100

    Récupération version Mi

--- private/view/header.php
+++ private/view/header.php
@@ -3,8 +3,8 @@
     <?php require_once('nav.php'); ?>
     <div id="logo"><a href="index.php"><img src="public/images/logo.svg"></a></div>
     <div id="tagline">
-        <h1>L'agence dont vous êtes le</h1>
-        <img src="public/images/heros.svg"></img>
+        <h1>L'agence web dont vous êtes le</h1>
+        <img src="public/images/hero2.png"></img>
     </div>
     <!-- <div id="btn-scroll" class="bounce"> -->
         <a id="btn-scroll" class="scrollTo bounce" href="#public">En savoir plus ;)</a>
