c7ef88f02dbc033ea19b5c0450084c358ee7cc52
mi <mi@localhost.localdomain>
2017-05-08 19:48:45 +0200

    modif textes et couleur

--- private/view/footer.php
+++ private/view/footer.php
@@ -1,15 +1,15 @@
         <footer>
             <section class="socials" id="socials">
                 <div>
-                    <a href="http://blog.mpaw.xyz" title="Rejoindre le blog">Le blog</a>
+                    <a href="http://blog.mpaw.xyz" target="_blank" title="Rejoindre le blog">Le blog</a>
                 </div>
                 <div> | </div>
                 <div>
-                    <a href="https://www.facebook.com/MaPetiteAgenceWeb/" target="blank"><img src="public/images/logoFB.svg" width="40rem" alt="Suivez-nous sur facebook" /></a>
+                    <a href="https://www.facebook.com/MaPetiteAgenceWeb/" target="_blank"><img src="public/images/logoFB.svg" width="40rem" alt="Suivez-nous sur facebook" /></a>
                 </div>
             </section>
-            <p><img src="public/images/pinapple-icon.png" width="30rem">
-            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="blank">CC BY-NC-SA 4.0</a> - 2017 | Réalisé par <a href="http://www.cooldev.xyz">Lionel</a> pour <a href="http://www.mpaw.xyz" target="blank">MPAW </a><img src="public/images/pinapple-icon.png" width="30rem"></br>
+            <p><img src="public/images/pinapple-icon.png" width="20rem">
+            <a href="https://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a> - 2017 | Réalisé par <a href="http://www.cooldev.xyz">Cooldev</a><img src="public/images/pinapple-icon.png" width="20rem"></br>
             <a href="ml.php">Mentions légales</a></p>
         </footer>
         <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
--- private/view/main.php
+++ private/view/main.php
@@ -3,49 +3,52 @@
         <div class="section-header">
         </div>
         <div class="section-content">
-            <h2>
-		MPAW, communauté apprenante de la communication digitale
+            <h2><mark>
+		Développez vos compétences <br/>en communication digitale</mark>
             </h2>
             <p>
-                Pour les associations, les entrepreneurs qui démarrent et n'ont pas de gros budgets, Ma Petite Agence Web est une association qui vous amène vers l'autonomie dans votre communication digitale.
-            </p>
-            <p>
-                Pour les étudiants des métiers du web et les professionels qui débutent, MPAW leur permet de consolider leurs compétences par la transmission et l'application.
-            </p>
-            <p>
-                Nous prônons le DIY ( Do it Yourself = Faites le vous-même ) et le DIWO ( Do It With Others = Faites le avec d'autres )
+            Vous <strong>démarrez votre activité</strong> et vous souhaitez avoir une <strong>présence sur le web</strong>.<br/></br>
+Le problème, c'est que faire faire par un pro, ça a un coût souvent trop élevé ( mais justifié ) quand on commence.
+Alors vous vous dites que vous allez la faire vous-même.<br/> <br/>C'est une bonne idée !<br/><br/> Le prix moyen d'un site web réalisé soi-mếme, c'est entre 0€ et 50 €. Parfais ! C'est dans votre budget !<br/><br/>
+Mais voilà, si votre ambition est d'avoir un joli logo sur un site web responsive, dans l'air du temps, cela a un autre coût quand on réalise son site soi-même : du temps et de la persévérance. On le sait, on est passé par là...<br/><br/>
+
+C'est de là qu'est née Ma petite agence web. De l'envie de partager les galères et les bons plans, pour rendre le web plus beau et votre vie d'entrepreneur un chouilla plus facile ;)
             </p>
         </div>
     </section>
-    <section id="ateliers">
+    <section id="blog">
     	<div class="section-header">
     	</div>
         <div class="section-content">
-            <h2>
-		Devenez autonome dans votre communication digitale
+            <h2><mark>
+		Devenez autonome dans votre communication digitale</mark>
             </h2>
             <p>
-                Des articles et des ateliers pour découvrir les outils de communication libres et/ou gratuits disponibles
+                Des astuces, des tutos, du vécu pour vous aider dans votre communication web. Découvrez par exemple les outils de communication libres et/ou gratuits disponibles
             </p>
-            <a href="http://blog.mpaw.xyz">Le blog</a>
+            <a href="http://blog.mpaw.xyz"target="_blank">Le blog</a>
             <p>
 
-                Suivez l'actu et les prochains évènements sur notre page Facebook
+                Suivez-nous sur Facebook. Nous y mettons régulièrement les ressources que nous trouvons pertinentes pour vous
             </p>
-            <a href="https://www.facebook.com/MaPetiteAgenceWeb/" target="blank"><img src="public/images/logoFB.svg" width="40rem" alt="Suivez-nous sur facebook" /></a>
+            <a href="https://www.facebook.com/MaPetiteAgenceWeb/" target="_blank"><img src="public/images/logoFB.svg" width="40rem" alt="Suivez-nous sur facebook" /></a>
         </div>
     </section>
-    <section id="meetup">
+    <section id="ateliers">
         <div class="section-header">
     	  </div>
         <div class="section-content">
             <h2>
-		Aidez et / ou faîtes-vous aider
+		Accompagnement et conseils en communication web
             </h2>
+          
             <p>
-                Besoin d'être accompagné dans votre stratégie de communication ? Nous organisons régulièrement des rencontres entre apprenants.((bientôt))
+                Besoin d'être accompagné dans votre stratégie de communication ? (Parce qu'avouez, parfois c'est compliqué...) <br/><br/>Parlez-nous de votre projet, nous construirons ensemble, votre stratégie web.</p>
+				<a href="index.php#formContact" >Envoyer un message à l'équipe</a>
+				<p>	                
+                ((bientôt)) Nous organisons régulièrement des ateliers pour s'approprier des outils qui vous faciliteront la vie. 
             </p>
-            <a href="#" target="blank"><img src="public/images/meetup-logo1200x630-transparent.png" width="" alt="Suivez-nous sur Meetup" /></a>
+            <a href="#" target="_blank"><img src="public/images/meetup-logo1200x630-transparent.png" width="300px" alt="Suivez-nous sur Meetup" /></a>
         </div>
     </section>
     <section id="pro">
@@ -56,9 +59,7 @@
 	...et si vous avez besoin de plus, faîtes faire !
           </h2>
           <p>
-              Grâce à la communauté MPAW, vous pouvez faire réaliser des supports de communication. Nous proposons nos services pour les personnes qui n'ont pas accès aux services d'une agence web classique (demandeurs d'emploi en création d'entreprise, personnes au RSA) à tarifs solidaires. <br/>
-              Nous réalisons également tous types de projets web. En choisissant Ma Petite Agence Web pour la réalisation de votre site internet, vous permettez à d'autres personnes d'accéder à la communication digitale et donnez plus de chances à leurs projets !  <br/>
-              Nous pouvons aussi vous mettre en relation avec des professionnels du territoire qui ont signé la charte des gentils.<br/><br/>
+              Nous réalisons également tous types de projets web en collaboration avec l'agence <a id="lien-classic" href="http://www.cooldev.xyz" target="_blank">Cooldev</a>  <br/><br/>
               Parlez-nous de votre projet !
           </p>
       </div>
